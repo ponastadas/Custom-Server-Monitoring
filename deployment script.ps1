93b77ef108b94492be969b0b2726118d51e3809c
tadasmo <tadas.mockevicius@gmail.com>
2023-03-20 22:08:52 +0200

    Update deployment script.ps1
    added backup

--- deployment script.ps1
+++ deployment script.ps1
@@ -4,9 +4,17 @@ $installScriptUrl = 'https://raw.githubusercontent.com/ponastadas/Custom-Server-
 # Define the path for the install script
 $installScriptPath = '/home/monitoring/install.ps1'
 
+# Define the path for the report file
+$reportFilePath = '/var/www/html/report.html'
+
 # Create the directory /home/monitoring if it doesn't already exist
 if (-not (Test-Path '/home/monitoring')) {
     New-Item -ItemType Directory -Path '/home/monitoring'
+}
+
+# Check if the report file exists and make a backup copy if it does
+if (Test-Path $reportFilePath) {
+    Copy-Item $reportFilePath "$reportFilePath.bak"
 }
 
 # Download the install script and save it to /home/monitoring/install.ps1
