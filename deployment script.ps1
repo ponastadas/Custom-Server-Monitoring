d96747939a01638acea464891c53d93febb3100b
tadasmo <tadas.mockevicius@gmail.com>
2023-03-20 22:14:11 +0200

    backup destination

--- deployment script.ps1
+++ deployment script.ps1
@@ -7,14 +7,25 @@ $installScriptPath = '/home/monitoring/install.ps1'
 # Define the path for the report file
 $reportFilePath = '/var/www/html/report.html'
 
+# Define the path for the backup directory
+$backupDirectory = '/home/monitoring/backup'
+
 # Create the directory /home/monitoring if it doesn't already exist
 if (-not (Test-Path '/home/monitoring')) {
     New-Item -ItemType Directory -Path '/home/monitoring'
 }
 
+# Create the backup directory if it doesn't already exist
+if (-not (Test-Path $backupDirectory)) {
+    New-Item -ItemType Directory -Path $backupDirectory
+}
+
+# Create a backup file name with the date-time stamp
+$backupFileName = '{0:yyyy-MM-dd_HH-mm-ss}_report.html.bak' -f (Get-Date)
+
 # Check if the report file exists and make a backup copy if it does
 if (Test-Path $reportFilePath) {
-    Copy-Item $reportFilePath "$reportFilePath.bak"
+    Copy-Item $reportFilePath "$backupDirectory/$backupFileName"
 }
 
 # Download the install script and save it to /home/monitoring/install.ps1
